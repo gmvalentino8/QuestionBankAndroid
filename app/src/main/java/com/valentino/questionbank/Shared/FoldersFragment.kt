42a79e07c3ae9c4d0ca208eb915cf4eac19d38c0
Marco Valentino <gmvalentino8@gmail.com>
2018-04-14 22:30:53 -0500

    First commit

---
+++ app/src/main/java/com/valentino/questionbank/Shared/FoldersFragment.kt
@@ -0,0 +1,25 @@
+package com.valentino.questionbank.Shared
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.valentino.questionbank.R
+
+class FoldersFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_folders, container, false)
+    }
+
+}
